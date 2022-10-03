ea1f41b16e0ac067507971991b6ceb707d17e00c
Mateusz Wargacki <mateuszw7@nycstudents.net>
2022-10-03 12:29:19 -0400

    Initial Commit

---
+++ src/Rectangle.java
@@ -0,0 +1,40 @@
+public class Rectangle {
+
+    // instance variables
+    private int length;
+    private int width;
+
+    // constructor to create a Rectangle object with a particular width and length
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // constructor to create a Rectangle object with equal width and length (a square)
+    public Rectangle(int side) {
+        length = side;
+        width = side;
+    }
+
+    // "no-argument" constructor which creates a Rectangle object with default values
+    public Rectangle() {
+        length = 100;
+        width = 50;
+    }
+
+    // "getter" method that returns the length of the Rectangle
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() { return width; }
+
+    public int getArea() { return length * width; }
+    // "setter" methods that sets the length of the Rectangle to a new length
+    public void setLength(int newLength) {
+        length = newLength;
+    }
+    public void setWidth(int newWidth) {
+        width = newWidth;
+}
+}
